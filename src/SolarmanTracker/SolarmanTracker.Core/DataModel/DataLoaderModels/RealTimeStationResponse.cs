846525426a37b66f8da29241eb55fb6804d34fa3
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2026-02-16 09:41:31 +0200

    correct model

--- src/SolarmanTracker/SolarmanTracker.Core/DataModel/DataLoaderModels/RealTimeStationResponse.cs
+++ src/SolarmanTracker/SolarmanTracker.Core/DataModel/DataLoaderModels/RealTimeStationResponse.cs
@@ -9,28 +9,28 @@ namespace SolarmanTracker.Core.DataModel.DataLoaderModels
 
         public float generationTotal { get; set; }
 
-        public float usePower { get; set; }
+        public float? usePower { get; set; }
 
-        public float wirePower { get; set; }
+        public float? wirePower { get; set; }
 
-        public float chargePower { get; set; }
+        public float? chargePower { get; set; }
 
-        public float dischargePower { get; set; }
+        public float? dischargePower { get; set; }
 
         [JsonConverter(typeof(UnixTimestampConverter))]
         public DateTime lastUpdateTime { get; set; }
 
-        public float generationPower { get; set; }
+        public float? generationPower { get; set; }
 
         public float? gridPower { get; set; }
 
-        public float purchasePower { get; set; }
+        public float? purchasePower { get; set; }
 
-        public float batteryPower { get; set; }
+        public float? batteryPower { get; set; }
 
         public bool success { get; set; }
 
         [JsonIgnore]
-        public bool isElectricityPresent => wirePower > 0;
+        public bool isElectricityPresent => wirePower.HasValue && wirePower.Value > 0;
     }
 }
